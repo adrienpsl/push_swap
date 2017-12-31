95dbe92650b1f70a6c509fa4078646523d9d2914
Adrien PUSEL <adpusel@student.42.fr>
2017-12-31 17:56:20 +0100

    save

---
+++ push_swap/push_swap.h
@@ -0,0 +1,19 @@
+//
+// Created by Adrien PUSEL on 12/31/17.
+//
+
+#ifndef PUSH_SWAP_PUSH_SWAP_H
+# define PUSH_SWAP_PUSH_SWAP_H
+
+# include "../../libft/includes/libft.h"
+
+/*
+**  ==============  push swap  ======================================
+*/
+
+/*
+**  		===========  main  ===========
+*/
+long	ft_atol(const char *s);
+
+#endif
