10dc401e89016b658e8f2b9fc960d1fe7bccc8f4
Adrien PUSEL <adpusel@student.42.fr>
2018-05-25 10:14:20 +0200

    save

--- rendu/correction_script.sh
+++ rendu/correction_script.sh
@@ -12,6 +12,16 @@ function err ()
 	fi
 }
 
+function err2 ()
+{
+	if [ "$1" = "Error" ];
+		then
+		true
+	else
+		echo "-------     $1" 
+	fi
+}
+
 err a
 err " a"
 
@@ -21,7 +31,18 @@ err "444d 55"
 err a111
 err "a111 887"
 
-# err 44 22 44
+err2 -111-
+err "-111-"
+
+a=`./checker 55 44 44` ; err2 $a
 err "55 44 22 44"
 
-# err 14165413131; 
+err "14 4 44 4564654646465"
+a=`./checker 14 4 44 4564654646465` ; err2 $a
+
+./checker
+
+# while true; do; ARG=`ruby -e "puts (-100..10000).to_a.shuffle.join(' ')"` | ./push_swap $ARG | ./checker $ARG ; done
+# while 1; do; leaks checker | grep "Process" | grep "leaks"; done
+
+
